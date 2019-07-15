2970c4b581c29917661fb218b4fd8eab04201874
Erick Simões <erick.simoes@live.com>
2019-07-15 17:52:11 -0300

    Add interaction script to exit of application

---
+++ Assets/Scripts/Interaction.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour {
+
+    private bool canExit = false;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        if (Input.GetKey(KeyCode.Escape)) {
+            Exit();
+        }
+    }
+
+    private void Exit() {
+        if (canExit) {
+            Application.Quit();
+        } else {
+            StartCoroutine(Countdown());
+            canExit = true;
+        }
+    }
+
+    IEnumerator Countdown() {
+        yield return new WaitForSeconds(2f);
+        canExit = false;
+    }
+}
